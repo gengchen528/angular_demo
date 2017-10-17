dc781791cfca13674698e8b1eeb30ae853e75a95
chengeng <geng.chen@datatist.com>
2017-10-17 11:02:48 +0800

    添加常用js函数

--- self_lib/self_lib.js
+++ self_lib/self_lib.js
@@ -7,8 +7,63 @@
 
 // 一、js常用
 
-
-
+/**
+* 1.1 判断是否为数字
+* isNumber
+* @ param str 传参
+* */
+isNumber = function (str){
+    var n = Number(str);
+    if (!isNaN(n)){
+        return true;
+    }else{
+        return false;
+    }
+};
+/**
+*1.2 判断是否是对象
+* isObj
+* @ param obj 传参
+ */
+isObj= function (obj){
+    if (typeof obj==="object") {
+        return true;
+    }else{
+        return false;
+    }
+};
+/**
+ * 1.3 判断是否为空
+ * isEmpty
+ * @param obj 传参
+ */
+isEmpty= function (obj){
+    var obj=obj;
+    if(obj===""||obj===null||obj===undefined||obj==="null"||obj==="undefined"){
+        return true;
+    }else{
+        return false;
+    }
+};
+/**
+ * 1.4 数组去重
+ */
+Array.prototype.unique1 = function(){
+    var res = [this[0]];
+    for(var i = 1; i < this.length; i++){
+        var repeat = false;
+        for(var j = 0; j < res.length; j++){
+            if(this[i] === res[j]){
+                repeat = true;
+                break;
+            }
+        }
+        if(!repeat){
+            res.push(this[i]);
+        }
+    }
+    return res;
+};
 //二、处理浏览器兼容性问题
 
 //2.1 处理IE6-IE8不兼容forEach()与map()遍历
